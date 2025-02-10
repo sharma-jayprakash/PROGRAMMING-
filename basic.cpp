0019509b80a0b66b33f88d68c5154a4d5bb64951
sharma-jayprakash <178124387+sharma-jayprakash@users.noreply.github.com>
2025-02-10 11:37:04 +0530

    basic done with c++

--- basic.cpp
+++ basic.cpp
@@ -1,7 +1,133 @@
-#include<iostream>
-#include<bitstring.h>
-using namespace std; 
-int main()
-{
-cout<<"hello wrold"; 
-}
+// #include<iostream>
+// #include<bitstring.h>
+// using namespace std; 
+// int main()
+// {
+// cout<<"hello wrold"; 
+// }
+// #include<iostream>
+// #include<bitstring.h>
+// #include<iostream>
+// using namespace std; 
+// int main(){
+// cout<<"hello pw "<<endl ; 
+// cout<<"hello jayprakash"<<"\n"; //its comments and endl is used to go to next line 
+// cout<<"hello competitave programming"; 
+// }
+
+//variable and number how to declare the variable 
+
+// variable are used to store the data and they can be update , intilised , and deleted at any time 
+
+
+// #include<iostream>
+// using namespace std; 
+// int main()
+// {
+    //  how to declare the string dataype , 
+//    string  y = "jayprakash"; 
+//     cout<<y ; 
+
+
+//  how to declare the int  dataype , 
+// int a ; // in c/c++ you can't change the datatype of varibale 
+// a= 5; 
+// //a="jayprakash ";  not possible , 
+// a = 6 ; //redeclare of a can be possible 
+// cout<<a ; 
+
+// //  how to declare the floor dataype , 
+// float a = 6.5 ;
+// a = 5;  
+// cout<<a<<"\n"; 
+
+// bool a = true; 
+// cout<<a<<endl; // gives the output in 0 and 1; 
+
+// int a = 8; 
+// string  b = "jayprakash"; 
+// cout<<a*b; // its is static type you can't multiply and divide the other datatypes ; 
+
+// int a=8; 
+ 
+// a+=10 ; //efficient way of doing shit , 
+// cout<<a;   
+
+// }
+
+//operators 
+// #include<iostream>
+// #include<bitstring.h>
+// using namespace std; 
+// int main(){
+//     int a= 7, b = 9  ; 
+//     // int a = 8 , b =  3; 
+// //     cout<<a+b <<endl; // 11
+// //      cout<<a-b<< endl ; //5
+// //       cout<<a*b <<endl ;//24
+  // cout<<a%b <<endl ;// its give the reminder of a%b;
+// //        cout<<a/b <<endl;//2 its divison  and gives the only value int
+// cout<< a <<endl; //7
+// cout<< a++<<endl; //7 phle 7 ko print  kro and then increment kro 
+// cout<<a<<endl; //now print value of 8 
+// cout<<b<<endl ; //9
+// cout<<++b; //phle value increment kro  10 and then print kro 
+
+// }
+
+//area of circle 
+//  #include<iostream>
+//  #include<bitstring.h>
+//  using namespace std; 
+//  int main(){
+//      int radius, area; //declaring the value of radius , 
+//     cout<<"enter the radius : " ; 
+
+//  cin>> radius ; 
+//  area = 3.14* radius *radius ; // this store the value of what we are going to get from this formula 
+//  cout<<" the area of circle is : "<<area; //this gonna print the area, 
+//  }
+
+///calculate the simple interest 
+//  #include <iostream>
+//  #include <bitstring.h>
+//  using namespace std; 
+//  int main(){
+//      int p, q, r, t ; 
+//      int simple_interest ; 
+//     cout<< "enter the principal amount : "; 
+//     cin >>p ; 
+//     cout<< "enter the rate :  "; 
+//      cin >>q; 
+
+//     cout<< "enter the time  : "; 
+//      cin >>r ; 
+
+//      simple_interest = (p*r*t)/100; 
+//      cout<<simple_interest<<endl;
+
+//  }
+
+//take a input as an int and then print its half , typecasting
+
+// #include<iostream>
+// using namespace std; 
+// int main(){
+//     int x; 
+//     cout<<"enter the value of x :"; 
+//     cin>>x; 
+//     float  y = (float)x; // now i am converting the value of x into y 
+//     cout<< y/2; // the problem i am getting is i only get half of which is divisible of 2 
+// }
+
+// heirarchy of computer mainly operators operators follow left to right  ,  and its have the same precendence of multiply and divide ,
+
+// //char datatypes ,  converting the value of a into int 
+// #include <iostream>
+// using namespace std; 
+// int main(){
+// char a; 
+// cout<<"enter the char value "; 
+// cin>>a; 
+// cout<<(int)a ; //using typecasting  of a , 
+// }
